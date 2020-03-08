51e79f84e5400ffb2229cf71f2c4f924815d7112
Maxime Meurisse <max.meurisse@outlook.com>
2020-03-08 09:37:21 +0100

    OShell v2 : minor fixes

--- oshell/src/oshell.c
+++ oshell/src/oshell.c
@@ -17,6 +17,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 #include <signal.h>
@@ -35,6 +36,9 @@
 
 // Flag to handle timeout
 int sigalrm_flag = 0;
+
+// PID to kill if timeout
+pid_t sigalrm_pid;
 
 
 /*************/
@@ -119,30 +123,60 @@ vector* cmd_loadmem(vector* cmd_list) {
     return vector_import(cmd_list);
 }
 
+/**
+ * Returns the input string where all multiple
+ * spaces have been replaced by only one space.
+ *
+ * @param   str         the input string
+ *
+ * @return  clean_str   the cleaned string
+ */
 static char* remove_multiple_space(char* str) {
+    // We get the length of the input string
     size_t str_length = strlen(str);
 
+    // We allocate a new string of the same length
     char* clean_str = malloc(sizeof(char) * (str_length + 1));
-    int j = 0;
-    int space_mode = 0;
 
+    if(clean_str == NULL)
+        return NULL;
+
+    // We initialize variables
+    int j = 0;
+    bool space_mode = false;
+
+    // We iterate over each char if the input string
     for(size_t i = 0; i < str_length; i++) {
+        // If the char is a space
         if(str[i] == ' ') {
+            // If we are not in the space mode
+            // (last character wasn't a space),
+            // we save the space and enter to
+            // the space mode (to do not save
+            // following spaces if any).
             if(!space_mode) {
                 clean_str[j++] = str[i];
-                space_mode = 1;
+                space_mode = true;
             }
-        } else {
+        }
+
+        // If the char is not a space, we save
+        // it and reset the space mode.
+        else {
             clean_str[j++] = str[i];
-            space_mode = 0;
+            space_mode = false;
         }
     }
 
+    // We properly terminate the string
     clean_str[j] = '\0';
 
     return clean_str;
 }
 
+/**
+ * Execute the 'sys netstats' command.
+ */
 static void cmd_sys_netstats() {
     FILE* fptr;
 
@@ -169,29 +203,38 @@ static void cmd_sys_netstats() {
 
     // We get and print the information, line by line
     while(fgets(buffer, 255, fptr)) {
+        // We clean the buffer (by removing multiple spaces)
         char* clean_buffer = remove_multiple_space(buffer);
 
+        if(clean_buffer == NULL) {
+            fprintf(stderr, "netstats : unable to read into buffer\n");
+
+            return;
+        }
+
+        // We scan the useful information
         sscanf(
             clean_buffer,
             "%[^:]: %*d %d %d %d %*u %*u %*u %*u %*u %d %d %d",
             ifname, &r_pkts, &r_err, &r_drop, &s_pkts, &s_err, &s_drop
         );
 
-        if(r_pkts != 0 || s_pkts != 0) {
-            r_err_rate = r_err / r_pkts;
-            r_drop_rate = r_drop / r_pkts;
-            s_err_rate = s_err / s_pkts;
-            s_drop_rate = s_drop / s_pkts;
-        } else {
-            r_err_rate = 0;
-            r_drop_rate = 0;
-            s_err_rate = 0;
-            s_drop_rate = 0;
-        }
+        // We calculate the rates
+        r_err_rate = r_pkts != 0 ? r_err / r_pkts : 0;
+        r_drop_rate = r_pkts != 0 ? r_drop / r_pkts : 0;
+        s_err_rate = s_pkts != 0 ? s_err / s_pkts : 0;
+        s_drop_rate = s_pkts != 0 ? s_drop / s_pkts : 0;
 
+        // We remove leading spaces before ifname
+        char* ifname_clean = ifname;
+
+        while(ifname_clean[0] == ' ')
+            ifname_clean++;
+
+        // We print the information
         printf(
             "[%s]: Rx(pkts: %d, err: %d%c, drop: %d%c) - Tx(pkts: %d, err: %d%c, drop: %d%c)\n",
-            ifname, r_pkts, r_err_rate, '%', r_drop_rate, '%', s_pkts, s_err_rate, '%', s_drop_rate, '%'
+            ifname_clean, r_pkts, r_err_rate, '%', r_drop_rate, '%', s_pkts, s_err_rate, '%', s_drop_rate, '%'
         );
 
         free(clean_buffer);
@@ -201,47 +244,80 @@ static void cmd_sys_netstats() {
     fclose(fptr);
 }
 
-static int is_symlink(const char *filename) {
+/**
+ * Returns a boolean indicating if the file
+ * 'filename' is a symlink or not.
+ *
+ * @param   filename    the path to a file
+ *
+ * @return              a boolean indicating if 'filename'
+ *                      is a symlink or not
+ */
+static bool is_symlink(const char *filename) {
     struct stat p_statbuf;
 
+    // We get file information
     if(lstat(filename, &p_statbuf) < 0) { 
-        perror("Check symlink");
-        exit(1); 
+        perror("netstats : unbale to get file information\n");
+
+        // We return 'true' to ignore the file by default
+        // (because symlink are ignored in the program)
+        return true;
     }
 
-    if(S_ISLNK(p_statbuf.st_mode) == 1)
-        return 1;
-    else
-        return 0;
+    // We check if the file is a symlink
+    return S_ISLNK(p_statbuf.st_mode);
 }
 
+/**
+ * Core execution of the 'sys devstats' command.
+ * It iterate over all devices and read information.
+ *
+ * @param   base_path       the path to directory to explore
+ * @param   n_active        the number of active devices
+ * @param   n_suspended     the number of suspended devices
+ * @param   n_unsupported   the number of unsupported devices
+ */
 static void get_device_info(char* base_path, int* n_active, int* n_suspended, int* n_unsupported) {
+    // We initialize variables
     char path[1000];
+
+    DIR *dir;
     struct dirent *dp;
-    DIR *dir = opendir(base_path);
 
-    if(!dir)
+    // We try to open the directory
+    if((dir = opendir(base_path)) == NULL) {
         return;
+    }
 
+    // We iterate over each element of the directory
     while((dp = readdir(dir)) != NULL) {
+        // We ignore "." and ".." directories
         if(strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+            // We construct the path to the current element
             strcpy(path, base_path);
             strcat(path, "/");
             strcat(path, dp->d_name);
 
+            // We check if the element is "runtime_status" file
+            // (because the information that we want is located
+            // in this file)
             if(strcmp(dp->d_name, "runtime_status") == 0) {
                 FILE* fptr;
 
+                // We try to open the file
                 if((fptr = fopen(path, "r")) == NULL) {
-                    fprintf(stderr, "Unable to get device information\n");
+                    fprintf(stderr, "Warning : unable to get device information\n");
 
                     return;
                 }
 
-                char buffer[12];
+                // We read the element in the file
+                char buffer[255];
 
-                fgets(buffer, 12, fptr);
+                fgets(buffer, 255, fptr);
 
+                // We check the read element and update the appropriate counter
                 if(strncmp(buffer, "active", strlen("active")) == 0) {
                     (*n_active)++;
                 } else if(strncmp(buffer, "suspended", strlen("suspended")) == 0) {
@@ -250,9 +326,11 @@ static void get_device_info(char* base_path, int* n_active, int* n_suspended, in
                     (*n_unsupported)++;
                 }
 
+                // We close the file
                 fclose(fptr);
             }
 
+            // If the element is not a symbolic link, we explore it
             if(!is_symlink(path)) {
                 get_device_info(path, n_active, n_suspended, n_unsupported);
             }
@@ -262,6 +340,9 @@ static void get_device_info(char* base_path, int* n_active, int* n_suspended, in
     closedir(dir);
 }
 
+/**
+ * Execute the 'sys devstats' command.
+ */
 static void cmd_sys_devstats() {
     // We initialize data that we wand to read
     int n_active = 0;
@@ -277,6 +358,14 @@ static void cmd_sys_devstats() {
     printf("Unsupported: %d\n", n_unsupported);
 }
 
+/**
+ * Returns the details about a state represented
+ * by a char.
+ *
+ * @param   state   a char representing a state
+ *
+ * @return          the details about the state
+ */
 static char* get_state_details(char state) {
     switch(state) {
         case 'R' : return "R (running)";
@@ -288,6 +377,12 @@ static char* get_state_details(char state) {
     }
 }
 
+/**
+ * Execute the 'sys stats <pid>' command.
+ *
+ @param     arguments   represents an array of string which contains the command ([0])
+ *                      and its arguments ([1], [2], ..., [255]).
+ */
 static void cmd_sys_stats(char** arguments) {
     /* missing argument */
     if(arguments[2] == NULL) {
@@ -391,7 +486,11 @@ void exec_once(char** arguments, vector* cmd_list) {
 static void sigalrm_handler(int sig) {
     fprintf(stderr, "Process timeout\n");
 
+    // We set the flag
     sigalrm_flag = sig;
+
+    // We kill the process
+    kill(sigalrm_pid, 9);
 }
 
 void exec_sequential(char** arguments, vector* cmd_list, int number) {
@@ -415,19 +514,16 @@ void exec_sequential(char** arguments, vector* cmd_list, int number) {
             signal(SIGALRM, sigalrm_handler);
             alarm(TIME_LIMIT);
 
+            sigalrm_pid = pid;
+
             // Status of the child process
             int status;
 
             // We wait for child or timeout
-            while((wait(&status)) != -1 && sigalrm_flag == 0);
+            while(sigalrm_flag == 0 && wait(&status) != -1);
 
             // We add the command to the command list (timeout or not)
             vector_add(cmd_list, arguments[0], pid, WEXITSTATUS(status));
-
-            // If a timeout occurs, we kill the child process
-            if(sigalrm_flag == 1) {
-                kill(pid, 9);
-            }
 
             // We reset the timeout
             alarm(0);
