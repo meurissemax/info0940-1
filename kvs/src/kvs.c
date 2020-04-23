0cb3326dd0bc868a9e6c4d4b0bcc7f1fc4da8c9b
Maxime Meurisse <max.meurisse@outlook.com>
2020-04-23 09:10:27 +0200

    KVS : final implementation

--- kvs/src/kvs.c
+++ kvs/src/kvs.c
@@ -1,61 +1,83 @@
+/**
+ * INFO0940-1 - Operating systems
+ * University of Liege
+ * Academic year 2019-2020
+ *
+ * Group 1
+ * 20161278 - MEURISSE Maxime
+ * 20162864 - VERMEYLEN Valentin
+ */
+
 #include <linux/slab.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
 
 #include "kvs.h"
 
-// NB : For this assignment we do not consider the final '\0' character of string ending, 
-// as it has been posted in the ecampus forum.
-
 asmlinkage long kvs_insert(const char *key, size_t keylen, const char *val, size_t vallen){
-    
+    // Variables declaration (C90 compliant).
+    Node* datastruct;
+    Node* addition;
+
+    // Check if key and val are allocated
+    if(!val){
+        printk(KERN_ERR "Insertion failed. The string 'val' was not allocated.\n");
+        return -1;
+    }
+    if(!key){
+        printk(KERN_ERR "Insertion failed. The key 'key' was not allocated.\n");
+        return -1;
+    }
+
     // 1. The key already exists in the KVS. We only update its field.
 
     // Create a Node to iterate the linked list.
-    struct Node* datastruct = NULL;
+    datastruct = NULL;
 
     // Iterate over all entries in the list.
     list_for_each_entry(datastruct, &kvsHead, kvsList){
-        // Key already exists
-        if(strcmp(datastruct->key, key) == 0){
+        // Key may already exist
+        if(strncmp(datastruct->key, key, keylen) == 0){
+            // Only the prefix of datastruct->keylen matched, so no match for this entry
+            if(keylen != datastruct->keylen)
+                continue;
+
+            // Free the field and reallocate it at the right size
             kfree(datastruct->val);
             datastruct->val = (char*) kmalloc(vallen*sizeof(char), GFP_KERNEL);
-            // The allocation failed
+
+            // The allocation failed. Delete the entry.
             if(!datastruct->val){
-                // Error message to add
+                printk(KERN_ERR "Allocation of 'val' failed.\n");
+                // Update the linked list
+                list_del(&datastruct->kvsList);
+
+                // free the Node
+                kfree(datastruct->key);
+                kfree(datastruct);
                 return -1;
             }
 
-            // No error in allocation, copy val into the field.
-
-            // We cannot read outside of the allocated string val, so only fill 
-            if(vallen > strlen(val)+1){
-                // TO DO. Delete all 'for' for allocation
-            }
-            // No padding needed, vallen may even be smaller than strlen(val)+1
-            else{
-                int i;
-                for(i = 0 ; i < vallen ; i++){
-                    datastruct->val[i] = val[i];
-                }
-            }
+            // No error in allocation, copy vallen characters of val into the field
+            strncpy(datastruct->val, val, vallen);
+            datastruct->vallen = vallen;
             return 0;
         }
     }
 
     // 2. We have to create a new node
 
-    struct Node* addition = (struct Node*) kmalloc(sizeof(struct Node),GFP_KERNEL);
+    addition = (Node*) kmalloc(sizeof(Node),GFP_KERNEL);
 
     if(!addition){
-        printk("Unable to create the entry. Memory may be full.\n"); // KERN_DEBUG ?
+        printk(KERN_ERR "Unable to create the entry. Memory may be full.\n");
         return -1;
     }
 
     addition->key = (char*) kmalloc(keylen*sizeof(char), GFP_KERNEL);
 
     if(!addition->key){
-        printk("Unable to create the entry. The allocation for the key has not succeeded.\n");
+        printk(KERN_ERR "Unable to create the entry. The allocation for the key has not succeeded.\n");
         kfree(addition);
         return -1;
     }
@@ -63,7 +85,7 @@ asmlinkage long kvs_insert(const char *key, size_t keylen, const char *val, size
     addition->val = (char*) kmalloc(vallen*sizeof(char), GFP_KERNEL);
 
     if(!addition->val){
-        printk("Unable to create the entry. The allocation for the value has not succeeded.\n");
+        printk(KERN_ERR "Unable to create the entry. The allocation for the value has not succeeded.\n");
         kfree(addition->key);
         kfree(addition);
         return -1;
@@ -73,66 +95,21 @@ asmlinkage long kvs_insert(const char *key, size_t keylen, const char *val, size
 
     addition->keylen = keylen;
     addition->vallen = vallen;
-    printk("here");
-    // We cannot go read above the space allocated to the string, so padding
-    if(keylen > strlen(key)+1){
-        int i;
-        int sizeKey = strlen(val) + 1;
-                for(i = 0 ; i < sizeKey ; i++){
-                    datastruct->key[i] = key[i];
-                }
-                for(i = sizeKey ; i < keylen ; i++){
-                    datastruct->key[i] = '\0';
-                }
-    }
-    // No padding required 
-    else{
-        printk("hoi");
-        int i;
-        for(i = 0 ; i < keylen ; i++){
-            printk("soin");
-            printk("%c",key[i]);
-            printk("%c",datastruct->key[i]);
-            datastruct->key[i] = key[i];
-        }
-    }
-    printk("hoi");
-    // The same is true for val
-    if(vallen > strlen(val)+1){
-        int i;
-        int sizeVal = strlen(val) + 1;
-        for(i = 0 ; i < sizeVal ; i++){
-            datastruct->val[i] = val[i];
-        }
-        for(i = sizeVal ; i < vallen ; i++){
-            datastruct->val[i] = '\0';
-        }
-    }
-    else{
-        int i;
-        for(i = 0 ; i < vallen ; i++){
-            datastruct->val[i] = val[i];
-        }
-    }
+    strncpy(addition->val, val, vallen);
+    strncpy(addition->key, key, keylen);
 
     INIT_LIST_HEAD(&addition->kvsList);
-    
-    /* Normally cannot happen
-    if(!addition->kvsList){
-        printk("Unable to create the entry.\n");
-        kfree(addition->val);
-        kfree(addition->key);
-        kfree(addition);
-        return -1;
-    }*/
-    printk("ip");
+
     // Add the node in the linked list.
     list_add(&addition->kvsList, &kvsHead);
     
     return 0;
 }
 
+
 asmlinkage long kvs_search(const char *key, size_t keylen, char *val, size_t max_vallen) {
+
+    Node* datastruct;
 
     if(!val){
         printk("Search failed. The buffer 'val' was not allocated.\n");
@@ -144,20 +121,25 @@ asmlinkage long kvs_search(const char *key, size_t keylen, char *val, size_t max
     }
 
     // Create a Node to iterate the linked list.
-    struct Node* datastruct = NULL;
+    datastruct = NULL;
     
     // Iterate over all entries in the list.
     list_for_each_entry(datastruct, &kvsHead, kvsList){
-        // We have the key in KVS.
-        if(strcmp(datastruct->key, key)==0){
-            // 1. The buffer is big enough
-            if(max_vallen >= datastruct->vallen){ // Should we add the '\0' character ? +1 ?
-                strncpy(val, datastruct->val, datastruct->vallen); // +1 ?
+        // We may have the key in KVS.
+        if(strncmp(datastruct->key, key, keylen)==0){
+            // Only the prefix of datastruct->keylen matched, so no match for this entry
+            if(keylen != datastruct->keylen)
+                continue;
+
+            // 1. Buffer large enough
+            if(max_vallen >= datastruct->vallen){
+                strncpy(val, datastruct->val, datastruct->vallen); 
                 return 1;
             }
-            // 2. The buffer is not big enough.
+            
+            // 2. The buffer is not large enough. We decided not to copy anything in it and return -1.
             else{
-                // What to do ?
+                printk(KERN_ERR "Key found but buffer not large enough to contain the value.\n");
                 return -1;
             }
         }
@@ -165,18 +147,37 @@ asmlinkage long kvs_search(const char *key, size_t keylen, char *val, size_t max
     return 0;
 }
 
+
 asmlinkage long kvs_delete(const char *key, size_t keylen) {
 
+    Node* datastruct;
+
+    // The pointer key is not allocated
+    if(! key){
+        printk(KERN_ERR "key pointer not allocated.\n");
+        return -1;
+    }
+
     // Create a Node to iterate the linked list.
-    struct Node* datastruct = NULL;
+    datastruct = NULL;
     // Iterate over all entries in the list.
     list_for_each_entry(datastruct, &kvsHead, kvsList){
-        // We find the key
-        if(strcmp(datastruct->key, key)==0){
+        // We may have found the key
+        if(strncmp(datastruct->key, key, keylen)==0){
+            // Only the prefix of datastruct->keylen matched, so no match for this entry
+            if(keylen != datastruct->keylen)
+                continue;
+
+            // Update the linked list
             list_del(&datastruct->kvsList);
+
+            // free the Node
+            kfree(datastruct->key);
+            kfree(datastruct->val);
+            kfree(datastruct);
             return 0;
         }
     }
-    // Add error management
+    // No key found, still return 0
     return 0;
 }
--- kvs/src/kvs.h
+++ kvs/src/kvs.h
@@ -1,19 +1,66 @@
+/**
+ * INFO0940-1 - Operating systems
+ * University of Liege
+ * Academic year 2019-2020
+ *
+ * Group 1
+ * 20161278 - MEURISSE Maxime
+ * 20162864 - VERMEYLEN Valentin
+ */
+
+#ifndef kvs_h
+#define kvs_h
+
 #include <linux/list.h> 
 
+// Pointer to the head of the linked list
 static LIST_HEAD(kvsHead);
 
-struct Node {
+// Structure defining a node in the linked list, containing an entry of the KVS
+typedef struct Node {
     char* key;
     char* val;
     size_t keylen;
     size_t vallen;
 
     struct list_head kvsList;
-};
+} Node;
 
-
+/**
+ * Inserts an entry in the KVS.
+ *
+ * @param 	key 		pointer to a key string to be stored.
+ * @param 	keylen  	length of the key string.
+ * @param   val         pointer to a value string to be stored.
+ * @param   vallen      length of the value string.
+ * 
+ * @return  0 on success, -1 on failure.						
+ */
 asmlinkage long kvs_insert(const char *key, size_t keylen, const char *val, size_t vallen);
 
+/**
+ * Searches for an entry in the KVS.
+ *
+ * @param 	key 		pointer to a key string to be searched.
+ * @param 	keylen  	length of the key string.
+ * @param   val         pointer to a buffer to receive the result value.
+ * @param   vallen      size of the 'val' buffer
+ * 
+ * @return  1 if a value is found for a specified key, 0 if not, -1 on failure.
+ * 
+ * NB : If the buffer is not big enough to receive the value, -1 is returned.		
+ */
 asmlinkage long kvs_search(const char *key, size_t keylen, char *val, size_t max_vallen);
 
+/**
+ * Deletes an entry in the KVS.
+ *
+ * @param 	key 		pointer to a key string to be deleted.
+ * @param 	keylen  	length of the key string.
+ * 
+ * @return  0 on success (deletion or nothing found for the given key),
+ *          -1 on failure.						
+ */
 asmlinkage long kvs_delete(const char *key, size_t keylen);
+
+#endif
